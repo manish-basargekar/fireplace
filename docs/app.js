0e8ccdb5c9f5ea452853752664dbdabda8f60400
calmighted <calmighted@gmail.com>
2021-03-10 14:36:23 +0530

    added music

--- docs/app.js
+++ docs/app.js
@@ -1,1 +1,19 @@
+const squirtle = [
+    
+]
 console.log("Its all vanilla My friend...");
+console.log("");
+let button = document.getElementById("play");
+let audio = document.getElementById("music");
+
+button.addEventListener("click", function(){
+  if(audio.paused){
+    audio.play();
+    button.innerHTML = "⏸️";
+    button.style.backgroundColor = "orangered"
+  } else {
+    audio.pause();
+    button.innerHTML = "🎵";
+    button.style.backgroundColor = "white"
+  }
+});
